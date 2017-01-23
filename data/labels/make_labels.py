061ed80158ca4272cec0caebbe8e5e5a6e07d801
Adam Hanna <ahanna@alumni.mines.edu>
2017-01-22 21:49:58 -0800

    removed data dir from git

--- data/labels/make_labels.py
+++
@@ -1,7 +0,0 @@
-import os
-
-l = ["ALB", "BET", "DOL", "LAG", "OTHER", "SHARK", "YFT"]
-
-for word in l:
-    print ("convert -fill black -background white -bordercolor white -border 4 -font Helvetica -pointsize 18 label:\"%s\" \"%s.png\""%(word, word))
-    os.system("convert -fill black -background white -bordercolor white -border 4 -font Helvetica -pointsize 18 label:\"%s\" \"%s.png\""%(word, word))
